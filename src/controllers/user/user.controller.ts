319cd626b70512f20e6b3908ad3328359ab51912
maurilio <maurilioz1@hotmail.com>
2021-03-01 17:38:54 -0300

    user-CRUD-db-integration
    Criado CRUD de usuário e integração com o banco de dados

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,17 +1,23 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/classes/user.entity';
 import { UserService } from 'src/services/user/user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService) {}
-    
+    constructor(private userService: UserService) { }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get()
+    async findAll(): Promise<User[]> {
+        return await this.userService.findAll();
+    }
+
     @UseGuards(AuthGuard('jwt'))
     @Get(':id')
-    async obterUsuario(@Param('id') identifier): Promise<User> {        
+    async obterUsuario(@Param('id') identifier): Promise<User> {
         var user = await this.userService.findOneById(identifier).then(user => user)
-        
+
         if (user)
             user.password = null;
 
@@ -24,7 +30,32 @@ export class UserController {
 
     @Post()
     async create(@Body() user: User) {
-
         return await this.userService.create(user);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() user: User) {
+
+        if (Number(id) !== user.id) {
+            console.log(id, user.id);
+            throw new HttpException("Os id's informados são diferentes", HttpStatus.NOT_FOUND)
+            
+        }
+
+        console.log(id, user.id);
+
+        return this.userService.update(id, user);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id) {
+
+        var user = await this.userService.findOneById(id).then(user => user);
+
+        if (!user) {
+            throw new HttpException("Nenhum usuário encontrado para essa solicitação", HttpStatus.NOT_FOUND)
+        }
+
+        await this.userService.remove(id);
     }
 }
